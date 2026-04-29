bc2a224adf59651acb5c3598e10e80c25e7f44ed
TriThuc2321 <trithuc23232@gmail.com>
2026-04-29 23:48:03 +0700

    feat: svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Button, useTheme } from '@heroui/react';
 
+import { LogoIcon } from './assets/icons';
 function App() {
   const { theme, setTheme } = useTheme();
   return (
-    <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-      {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
-    </Button>
+    <div>
+      <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
+      </Button>
+      <LogoIcon />
+    </div>
   );
 }
 
